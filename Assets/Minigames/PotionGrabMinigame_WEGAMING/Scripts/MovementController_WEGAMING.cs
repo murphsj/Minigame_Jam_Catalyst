5ebbc1b86d575cc27cc817d96d1c1c7338b14ce7
murphsj <j.murphy.samuel@gmail.com>
2025-09-20 18:45:24 -0400

    Adds character controller

--- Assets/Minigames/PotionGrabMinigame_WEGAMING/Scripts/MovementController_WEGAMING.cs
+++ Assets/Minigames/PotionGrabMinigame_WEGAMING/Scripts/MovementController_WEGAMING.cs
@@ -1,7 +1,9 @@
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 /// <summary>
-/// Handles movement and collision detection for the player.
+/// Handles collision detection for the player.
 /// Based on a character controller made by Sebastian Lague:
 /// <see cref="https://github.com/SebLague/2DPlatformer-Tutorial"/> 
 /// </summary>
@@ -10,10 +12,24 @@ public class MovementController_WEGAMING : MonoBehaviour
 {
     struct RaycastOrigins
     {
-        public Vector2 topLeft, topRight, bottomleft, bottomRight;
+        public Vector2 topLeft, topRight, bottomLeft, bottomRight;
     }
 
-    const float skinWidth = 0.15f;
+    public struct CollisionInfo
+    {
+        public bool left, right;
+        public bool above, below;
+
+        public void Reset()
+        {
+            left = right = false;
+            above = below = false;
+        }
+    }
+
+    public LayerMask collisionMask;
+
+    const float skinWidth = 0.015f;
 
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
@@ -21,8 +37,11 @@ public class MovementController_WEGAMING : MonoBehaviour
     float horizontalRaySpacing;
     float verticalRaySpacing;
 
-    BoxCollider2D collider;
+    new BoxCollider2D collider;
+
+    public CollisionInfo collisions;
     RaycastOrigins raycastOrigins;
+    
 
 
     void Start()
@@ -39,36 +58,82 @@ public class MovementController_WEGAMING : MonoBehaviour
     /// <param name="velocity">The distance to move</param>
     public void Move(Vector2 velocity)
     {
+        UpdateRaycastOrigins();
+        collisions.Reset();
 
+        if (velocity.x != 0)
+        {
+            HorizontalCollision(ref velocity);
+        }
+        if (velocity.y != 0)
+        {
+            VerticalCollision(ref velocity);
+        }
+
+        transform.Translate(velocity);
     }
 
     void HorizontalCollision(ref Vector2 velocity)
     {
-        
+        float directionX = Mathf.Sign (velocity.x);
+		float rayLength = Mathf.Abs (velocity.x) + skinWidth;
+		
+		for (int i = 0; i < horizontalRayCount; i ++)
+        {
+			Vector2 rayOrigin = (directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight;
+			rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+			Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength,Color.red);
+
+            if (hit)
+            {
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+
+                collisions.left = directionX == -1;
+                collisions.right = directionX == 1;
+			}
+		}
     }
 
     void VerticalCollision(ref Vector2 velocity)
     {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
 
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
+                
+            }
+        }
     }
 
-    /// <summary>
-    /// Recalculates raycast origins. Should be called if the controller size is updated
-    /// </summary>
     void UpdateRaycastOrigins()
     {
         Bounds bounds = collider.bounds;
         bounds.Expand(skinWidth * -2);
 
-        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.max.y);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.max.y);
-        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.min.y);
-        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
     }
 
-    /// <summary>
-    /// Calculates the spacing between raycasts based on ray count. Should be called if ray count is updated
-    /// </summary>
     void CalculateRaySpacing()
     {
         Bounds bounds = collider.bounds;
@@ -77,7 +142,7 @@ public class MovementController_WEGAMING : MonoBehaviour
         horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
         verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
 
-        horizontalRaySpacing = bounds.size.x / (horizontalRayCount - 1);
-        verticalRaySpacing = bounds.size.y / (verticalRayCount - 1);
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
     }
 }
--- Assets/Minigames/PotionGrabMinigame_WEGAMING/Scripts/Player_WEGAMING.cs
+++ Assets/Minigames/PotionGrabMinigame_WEGAMING/Scripts/Player_WEGAMING.cs
@@ -1,6 +1,68 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+/// <summary>
+/// Handles movement and input handling for the player.
+/// Based on a character controller made by Sebastian Lague:
+/// <see cref="https://github.com/SebLague/2DPlatformer-Tutorial"/> 
+/// </summary>
+[RequireComponent(typeof(BoxCollider2D))]
 public class Player_WEGAMING : MonoBehaviour
 {
-    
+    public float jumpHeight = 4;
+    public float timeToJumpApex = 0.4f;
+    public float moveSpeedGround = 6;
+    public float moveSpeedAir = 0;
+
+    float gravity;
+    float jumpVelocity;
+
+    Vector2 velocity;
+    Vector2 moveDirection;
+
+    MovementController_WEGAMING controller;
+
+    void Start()
+    {
+        controller = GetComponent<MovementController_WEGAMING>();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+
+    void OnInteract(InputValue inputValue)
+    {
+        if (!MinigameManager.IsReady()) return;
+
+        if (controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        if (!MinigameManager.IsReady()) return;
+        moveDirection = inputValue.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!MinigameManager.IsReady()) return;
+
+        if (controller.collisions.below || moveSpeedAir > 0)
+        {
+            velocity.x = moveDirection.x * (controller.collisions.below ? moveSpeedGround : moveSpeedAir);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+    }
+
+
 }
